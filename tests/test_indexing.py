5eb6ece5ab7238b651695c85e738b6ba8fbfd79e
= <nduldhardt@gmail.com>
2019-05-20 20:42:32 +0200

    Changed test to handle different file permissions inside travis docker

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -1,4 +1,5 @@
-from os import path
+import os
+import stat
 import unittest
 
 from pyback.checkpoint import Checkpoint
@@ -6,14 +7,14 @@ from pyback.checkpoint import Checkpoint
 
 class TestIndexing(unittest.TestCase):
     def setUp(self):
-        self.indexing_folder = path.join(
-                path.dirname(path.abspath(__file__)),
+        self.TEST_RESOURCES_PATH = os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
                 '../resources/tests/backup_indexing_test_folder'
                 )
 
     # TODO extend the test to verify that a correct checkpoint is built.
     def test_checkpoint_building(self):
-        checkpoint = Checkpoint.build_checkpoint(self.indexing_folder)
+        checkpoint = Checkpoint.build_checkpoint(self.TEST_RESOURCES_PATH)
                 
         expected_children_count = 6
         self.assertEqual(expected_children_count, len(checkpoint.root.children))
@@ -26,14 +27,15 @@ class TestIndexing(unittest.TestCase):
         if 'Microsoft' in platform.release():
             return
 
-        file = '100000_lines.md'
-        file_permissions = 0o644
+        file_name = '100000_lines.md'
+        file_path = os.path.join(self.TEST_RESOURCES_PATH, file_name)
+        file_permissions = stat.S_IMODE(os.lstat(file_path).st_mode)
 
-        symlink = '100000_lines_symlink'
-        symlink_permissions = 0o777
+        symlink_name = '100000_lines_symlink'
+        symlink_path = os.path.join(self.TEST_RESOURCES_PATH, symlink_name)
+        symlink_permissions = stat.S_IMODE(os.lstat(symlink_path).st_mode)
 
-        checkpoint = Checkpoint.build_checkpoint(self.indexing_folder)
+        checkpoint = Checkpoint.build_checkpoint(self.TEST_RESOURCES_PATH)
 
-        self.assertEqual(checkpoint.root.children[file].permissions, file_permissions)
-        self.assertEqual(checkpoint.root.children[symlink].permissions, symlink_permissions)
-
+        self.assertEqual(checkpoint.root.children[file_name].permissions, file_permissions)
+        self.assertEqual(checkpoint.root.children[symlink_name].permissions, symlink_permissions)
